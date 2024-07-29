921889be10f3d6bcc503d145313b6877abbb34d8
pmihailov <usernameispeter@gmail.com>
2024-07-29 22:06:15 +0400

    release

--- src/components/ButtonSpring/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ButtonSpring } from './ButtonSpring.tsx'
--- src/utils/misc.ts
+++
@@ -1,5 +0,0 @@
-import type { CodesRatio, CurrencyCode } from '../types/currencies'
-
-export const getPariRatio = (ratio: CodesRatio, code1: CurrencyCode, code2: CurrencyCode) => {
-  return ratio[code2] / ratio[code1]
-}
